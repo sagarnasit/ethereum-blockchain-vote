d30710d715eff8790fd49f47dd62c313eec2f993
Sagar <sagar.nasit@rtcamp.com>
2018-05-20 16:37:03 +0530

    Add Candidate Use struct to create candidate Add addCandidate method add candidateCount

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,28 @@ pragma solidity ^0.4.2;
 
 contract Election {
 
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidate
+    // Fetch Candidate
+    mapping( uint => Candidate ) public candidates;
+
+    // Store Cnadidate counts
+    uint public candidateCount;
+
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Sagar");
+        addCandidate("Ninad");
+    }
+
+    function addCandidate( string _name ) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate( candidateCount, _name, 0 );
     }
 }
